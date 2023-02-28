82664cd669a386ac67bccb5fe227fd20183230b8
prataprohit526 <prataprohit526@gmail.com>
2023-02-28 15:05:01 +0530

    add all

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,24 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [value, setvalue]=useState();
-    function showdata(){
-      setvalue("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
-    }
+  let [bool, setBool] = useState(false);
+  function showData() {
+      setBool(true);
+  }
   return (
-    <div id="main">
-      // Do not alter the main div
-      <button id="click" onClick={showdata}>on click</button>
-      <p id="para">{value}</p>
-    </div>
+      <div id="main">
+          <button id="click" onClick={showData}>
+              on click
+          </button>
+          {bool ? (
+              <p id="para">
+                  Hello, I've learnt to use the full-stack evaluation tool.
+                  This makes me so happy
+              </p>
+          ) : (
+              ""
+          )}
+      </div>
   );
 }
 
